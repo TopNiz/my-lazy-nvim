7d6a6338361ff1a1fa5202fe33c8295ad6de2f6f
Nizar S. AYED <nizarayed@plania.io>
2025-08-10 08:59:24 +0000

    stabilized lazy vim

--- init.lua
+++ init.lua
@@ -1,2 +1,8 @@
+-- ~/nvim/init.lua
+
+require("topniz.settings")
+require("topniz.lazy")
+require("topniz.maps") -- key bindings
+
 -- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+-- require("config.lazy")
